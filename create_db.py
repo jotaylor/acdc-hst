97e1082194441b645fae198e73dc609f6fc7363d
Jo Taylor <jotaylor@stsci.edu>
2020-04-30 17:57:58 -0400

    Removed schema information

--- create_db.py
+++ create_db.py
@@ -1,41 +1,15 @@
 import yaml
 from connect_db import load_connection
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column
-from sqlalchemy import Float, ForeignKey, Integer, String
+from schema import *
 
 with open("settings.yaml", "r") as f:
     SETTINGS = yaml.load(f)
-session, base, engine = load_connection(SETTINGS["connection_string"])
 
-class Darks(base):
-    """ORM for the header table"""
-
-    __tablename__ = 'darks'
-    id = Column(Integer, primary_key=True, nullable=False)
-    filename = Column(String(30))
-    segment = Column(String(5))
-    rootname = Column(String(10))
-    expstart = Column(Float())
-    exptime = Column(Float())
-    hva = Column(Integer())
-    hvb = Column(Integer())
-    fileloc = Column(String(200))
-    region = Column(String(10))
-    xcorr_min = Column(Integer())
-    xcorr_max = Column(Integer())
-    ycorr_max = Column(Integer())
-    ycorr_max = Column(Integer())
-    latitude = Column(Float())
-    longitude = Column(Float())
-    saa_flag = Column(Integer())
-    darkrate = Column(Float())
-    time = Column(Float())
-    unfiltered_pha_counts = Column(Float())
-
-def create_db():
-	base.metadata.create_all()
-	print("Created SQLite database ./cos_dark.db")
+def create_db(connection_string=SETTINGS["connection_string"]):
+    session, engine = load_connection(SETTINGS["connection_string"])
+    # It's important that the Base from schema.py be used (from the import)
+    Base.metadata.create_all(engine)
+    print("Created database cos_dark.db")
 
 if __name__ == "__main__":
     create_db()
