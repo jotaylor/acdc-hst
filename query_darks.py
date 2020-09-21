50158df940b96838aa40e8b9057613b4dacd9d93
Jo Taylor <jotaylor@stsci.edu>
2020-09-20 22:10:30 -0400

    Add new functions files_by_mjd, counts_by_mjd

--- query_darks.py
+++ query_darks.py
@@ -25,14 +25,15 @@ def all_darks(connection_string=SETTINGS["connection_string"]):
     session, engine = load_connection(connection_string)
 
     # Define columns to return from database
-    cols = ["expstart", "solar_flux"]
+    cols = ["expstart", "solar_flux", "latitude", "longitude"]
     cols += [f"dark_pha{x}" for x in range(0,32)]
     if TESTING is True: 
-        cols = ["expstart", "solar_flux", "dark_pha10"]
+        cols = ["expstart", "latitude", "longitude", "solar_flux", "dark_pha11"]
 
     # Execute SELECt query with WHERE statements
     query = session.query(Darks).options(load_only(*cols))\
-                .filter(Darks.region == "inner")
+                .filter(Darks.region == "inner")\
+                .filter(Darks.segment == "FUVA")
     if TESTING is True:
         query = query.filter(Darks.filename == "le0a3mcuq_corrtag_a.fits")
     results = query.all()
@@ -41,5 +42,43 @@ def all_darks(connection_string=SETTINGS["connection_string"]):
     d = {col: [getattr(x, col) for x in results] for col in cols}
     df = pd.DataFrame(d)
     
+    return df
+
+def files_by_mjd(mjdstart, mjdend, morecols=[],
+                 connection_string=SETTINGS["connection_string"]):
+    
+	# Connect to database
+    session, engine = load_connection(connection_string)
+
+    cols = ["fileloc", "segment"]
+
+    query = session.query(Darks.fileloc.distinct(), Darks.segment)\
+                .filter(Darks.expstart >= mjdstart)\
+                .filter(Darks.expstart < mjdend)
+    results = query.all()
+
+    d = {cols[i]: [x[i] for x in results] for i in range(len(cols))}
+    df = pd.DataFrame(d)
+
+    return df
+
+def counts_by_mjd(mjdstart, mjdend, morecols=[],
+                  connection_string=SETTINGS["connection_string"]):
+    
+	# Connect to database
+    session, engine = load_connection(connection_string)
+
+    cols = ["segment", "exptime", "hv"]
+    cols += [f"dark_pha{x}" for x in range(0,32)]
+    cols += morecols
+
+    query = session.query(Darks).options(load_only(*cols))\
+                .filter(Darks.expstart >= mjdstart)\
+                .filter(Darks.expstart < mjdend)
+    results = query.all()
+
+    d = {col: [getattr(x, col) for x in results] for col in cols}
+    df = pd.DataFrame(d)
+
     return df
 
