d988072b12b7cac4b172d21aadca67ca7c208f97
Jo Taylor <jotaylor@stsci.edu>
2020-09-23 17:38:43 -0400

    Use DB name rather than sqlalchemy connection string

--- connect_db.py
+++ connect_db.py
@@ -1,14 +1,17 @@
+import os
 from sqlalchemy.pool import NullPool
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-def load_connection(connection_string, echo=False):
+def load_connection(dbname, echo=False):
     """
     Return session, base, and engine objects for connecting to the database.
 
     Args:
-        connection_string (str): The connection string to connect to the 
-            database. The connection string should take the form:
+        dbname (str): The location of the SQLite database, with full path, e.g.
+            /path/to/cos_dark.db 
+            If in the current directory, do not include . or ./ 
+            This will get plugged into the full connection string of form:
             `dialect+driver://username:password@host:port/database`
         echo (Bool): If True, log all statements to STDOUT. Defaults to False.
 
@@ -19,6 +22,7 @@ def load_connection(connection_string, echo=False):
             connectivity and behavior.
     """
 
+    connection_string = os.path.join("sqlite:///", dbname)
     engine = create_engine(connection_string, echo=echo, poolclass=NullPool)
     Session = sessionmaker(bind=engine)
     session = Session()
