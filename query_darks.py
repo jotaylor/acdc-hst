68347302a9232e7f228e0abef61267a106f0db20
Jo Taylor <jotaylor@stsci.edu>
2020-09-23 17:40:57 -0400

    Use DB name rather than SQLAlchemy connection string, add more columns to all_darks() and search across all obsmodes, add segment criteria to files_by_mjd

--- query_darks.py
+++ query_darks.py
@@ -8,32 +8,34 @@ from schema import Solar, Darks
 TESTING = False
 
 with open("settings.yaml", "r") as f:
-    SETTINGS = yaml.load(f)
+    SETTINGS = yaml.safe_load(f)
+    DBNAME = SETTINGS["dbname"]
 
-def all_darks(connection_string=SETTINGS["connection_string"]):
+def all_darks(dbname=DBNAME):
     """
     Query the Darks table to get darks for all time for all PHAs.
 
     Args:
-        connection_string (str): Connection string to DB of the form:
-            `dialect+driver://username:password@host:port/database`
+        dbname (str): The location of the SQLite database, with full path, e.g.
+            /path/to/cos_dark.db 
+            If in the current directory, do not include . or ./ 
     Returns:
         df (:obj:`pandas.DataFrame`): Query results.
     """
     
 	# Connect to database
-    session, engine = load_connection(connection_string)
+    session, engine = load_connection(dbname)
 
     # Define columns to return from database
-    cols = ["expstart", "solar_flux", "latitude", "longitude"]
+    cols = ["expstart", "solar_flux", "latitude", "longitude", "segment", "hv", "region"]
     cols += [f"dark_pha{x}" for x in range(0,32)]
     if TESTING is True: 
         cols = ["expstart", "latitude", "longitude", "solar_flux", "dark_pha11"]
 
     # Execute SELECt query with WHERE statements
-    query = session.query(Darks).options(load_only(*cols))\
-                .filter(Darks.region == "inner")\
-                .filter(Darks.segment == "FUVA")
+    query = session.query(Darks).options(load_only(*cols))
+#                .filter(Darks.region == "inner")\
+#                .filter(Darks.segment == "FUVA")
     if TESTING is True:
         query = query.filter(Darks.filename == "le0a3mcuq_corrtag_a.fits")
     results = query.all()
@@ -44,29 +46,31 @@ def all_darks(connection_string=SETTINGS["connection_string"]):
     
     return df
 
-def files_by_mjd(mjdstart, mjdend, morecols=[],
-                 connection_string=SETTINGS["connection_string"]):
+def files_by_mjd(mjdstart, mjdend, segment="FUVA", morecols=[],
+                 dbname=DBNAME):
     
 	# Connect to database
-    session, engine = load_connection(connection_string)
+    session, engine = load_connection(dbname)
 
-    cols = ["fileloc", "segment"]
+    cols = ["fileloc"]
 
-    query = session.query(Darks.fileloc.distinct(), Darks.segment)\
+    query = session.query(Darks.fileloc.distinct())\
                 .filter(Darks.expstart >= mjdstart)\
-                .filter(Darks.expstart < mjdend)
+                .filter(Darks.expstart < mjdend)\
+                .filter(Darks.segment == segment)\
+                .order_by(Darks.expstart)
     results = query.all()
-
+    
     d = {cols[i]: [x[i] for x in results] for i in range(len(cols))}
     df = pd.DataFrame(d)
 
     return df
 
 def counts_by_mjd(mjdstart, mjdend, morecols=[],
-                  connection_string=SETTINGS["connection_string"]):
+                  dbname=DBNAME):
     
 	# Connect to database
-    session, engine = load_connection(connection_string)
+    session, engine = load_connection(dbname)
 
     cols = ["segment", "exptime", "hv"]
     cols += [f"dark_pha{x}" for x in range(0,32)]
