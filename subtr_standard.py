dfcbcc225d254dd66f7c1c8a0dfbe85b7e1dcc49
Jo Taylor <jotaylor@stsci.edu>
2021-07-15 17:07:48 -0400

    Rewrite to be modular, PEP8 compliant

--- subtr_standard.py
+++ subtr_standard.py
@@ -1,145 +1,132 @@
+import argparse
+import copy
+import asdf
 from astropy.io import fits
-import sys
-from scipy.optimize import minimize
-from math import *
 import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 import glob
-plt.rc('font', family='serif')
-mpl.rcParams.update({'font.size': 12})
-mpl.rcParams.update({'legend.labelspacing':0.25, 'legend.fontsize': 12})
-mpl.rcParams.update({'errorbar.capsize': 4})
+import os
 
+from predict_dark_level1 import bin_science, get_binning_pars
 
-def read_and_convert_science(filename, fact):
-	hdul = fits.open(filename)
-	data = hdul[1].data
-
-	xstart = 1264
-	xend   = 15112
-	ystart = 376
-	yend   = 660
-
-	#val1 = np.zeros ((12, 2312)) 
-	#n_rec = np.zeros ((12, 2312))
-	val1 = np.zeros ((12, 289*fact)) 
-	n_rec = np.zeros ((12, 289*fact))
-
-	## Reading corrtag file and preparing binned image 
-	for i in range (0, len(data['XCORR'][:])):
-		if (data['XCORR'][i] > xstart) and (data['XCORR'][i] < xend-6):
-			if (data['YCORR'][i] > ystart) and (data['YCORR'][i] < yend) and (data['PHA'][i] < 28) and (data['PHA'][i] > 3):
-				jj = int ((data['XCORR'][i] - xstart) / 6.0 / 8.0 * fact)
-				kk = int ((data['YCORR'][i] - ystart) / 12.0 / 2.0)
-				if ((int(data['PHA'][i]) > 3) and (int(data['PHA'][i]) <28)): 
-					val1 [kk, jj]  = val1[kk, jj] + data['EPSILON'][i]	
-					n_rec [kk, jj] = n_rec[kk, jj] + 1.0
-
-	return [val1, n_rec]	
-
-
-fact = 2
-
-
-sciences = glob.glob('/Users/sveash/Dropbox/COS_dark/low_snr/M83-1/ldaw01a7q*corrtag_a.fits')
-rootdir = '/Users/sveash/Dropbox/COS_dark/low_snr/M83-1/dark_corr/v2/'
-
-for s in sciences:
-        ## get rootname of file
-        rootn = s.split('-1/')[1].split('_corr')[0]
+def main(corrtags, pred_noise_file, fact=2, outdir="."):
+# TO DO what is fact?
+    pred_noise_af = asdf.open(pred_noise_file)
+    pred_noise = pred_noise_af["3-29"]
+    b = get_binning_pars(pred_noise_af)
+    for item in corrtags:
+        binned, nevents = bin_science(item, b, fact)
         
-        ## Read predicted noise level
-        noise = np.load(f'{rootdir}{rootn}_noise_complete.npy')
+        hdulist = fits.open(item, mode="update")
+        data = hdulist[1].data
         
-        ## Read science exposure
-        signal, n_rec = read_and_convert_science (s, fact)
-        print('Size signal: ', len(signal[0]))
+        xstart = b["xstart"]
+        xend = b["xend"]
+        ystart = b["ystart"]
+        yend = b["yend"]
+        phastart = b["phastart"]
+        phaend = b["phaend"]
+        bin_x = b["bin_x"]
+        bin_y = b["bin_y"]
 
-        hdul = fits.open(s) 
-        data = hdul[1].data
+        wwf = open("tst.txt", "w")
+        logic = np.zeros((binned.shape[0], binned.shape[1]))
+        
+        all_delta_eps_ind = []
+        for i in range(len(data["xcorr"])):
+            if data["xcorr"][i] > xstart and data["xcorr"][i] < xend:
+                if data["ycorr"][i] > ystart and data["ycorr"][i] < yend:
+                    if data["pha"][i] > phastart and data["pha"][i] < phaend:
+                        x = int((data["xcorr"][i] - xstart) // bin_x * fact)
+                        y = int((data["ycorr"][i] - ystart) // bin_y)
+                        delta_eps = pred_noise[y, int(x/fact)] / float(fact)
+                        logic[y,x] = 1
+                        delta_eps_ind = delta_eps / float(nevents[y,x])
+                        outstr = f"{data['epsilon'][i]}\t{delta_eps}\t{delta_eps_ind}\n"
+                        data["epsilon"][i] -= delta_eps_ind
+                        wwf.write(outstr)
+        wwf.close()
 
-        xstart = 1264
-        xend   = 15112
-        ystart = 376
-        yend   = 660
+        plt.imshow(logic, aspect="auto", origin="lower")
+        plt.savefig("logic.png")
+        plt.clf()
+        
+#        hdulist = fits.open(item, mode="update")
+#        data = hdulist[1].data
+#        phainds = np.where((data["pha"] >= b["phastart"]) &
+#                           (data["pha"] <= b["phaend"]))
+#        phadata = data[phainds]
+#        innerinds = np.where((phadata["xcorr"] > b["xstart"]) &
+#                             (phadata["xcorr"] < b["xend"]) &
+#                             (phadata["ycorr"] > b["ystart"]) &
+#                             (phadata["ycorr"] < b["yend"]))
+#        filtered = phadata[innerinds]
+#
+#        wwf = open("tst.txt", "w")
+#        logic = np.zeros((binned.shape[0], binned.shape[1]))
+##        xs = (filtered["xcorr"] - b["xstart"]) // b["bin_x"] * fact
+##        xs = (filtered["ycorr"] - b["ystart"]) // b["bin_y"]
+##        delta_eps = pred_noise[
+#        print("before loop 1")
+#        all_delta_eps_ind = []
+#        for i in range(len(filtered["xcorr"])):
+#            print(i)
+#            x = int((filtered["xcorr"][i] - b["xstart"]) // b["bin_x"] * fact)
+#            y = int((filtered["ycorr"][i] - b["ystart"]) // b["bin_y"])
+#            delta_eps = pred_noise[y, int(x/fact)] / float(fact)
+#            logic[y,x] = 1
+#            delta_eps_ind = delta_eps / float(nevents[y,x])
+##            all_delta_eps_ind.append(delta_eps_ind)
+#            outstr = f"{data[phainds][innerinds][i]}\t{delta_eps}\t{delta_eps_ind}\n"
+#        #    data[phainds][innerinds]["epsilon"][i] -= delta_eps_ind
+#            filtered["epsilon"][i] -= delta_eps_ind
+#            wwf.write(outstr)
+#        
+#        data[phainds][innerinds]["epsilon"] = all_delta_eps_ind
+##        data[phainds][innerinds]["epsilon"] -= all_delta_eps_ind
+#        print("after loop 1")
+#        wwf.close()
+#
+#        plt.imshow(logic, aspect="auto", origin="lower")
+#        plt.savefig("logic.png")
+#        plt.clf()
 
-        wwf = open ('tst.txt', 'w')
+        
+        inds = np.where(logic < 1)
+        import pdb; pdb.set_trace()
+        if len(logic[inds]) != 0:
+# TO DO what about DQ?
+            new_events = copy.deepcopy(data[len(data) - len(inds[0]):])
+            for i in range(len(inds[0])):
+                y = b["bin_y"] * inds[0][i] + b["ystart"] 
+                x = b["bin_x"] * inds[1][i] + b["xstart"]
+                new_events["rawx"][i] = x 
+                new_events["rawy"][i] = y
+                new_events["xcorr"][i] = x 
+                new_events["ycorr"][i] = y
+                new_events["xfull"][i] = x 
+                new_events["yfull"][i] = y
+                new_events["pha"][i] = 10
+                new_events["epsilon"][i] = -pred_noise[inds[0][i], int(inds[1][i]/fact)] / fact
+                 
+            data = np.concatenate([data, new_events])
+        hdulist[1].data = data
+        outfile = os.path.join(outdir, f"corrected_{os.path.basename(item)}")
+        hdulist.writeto(outfile, overwrite=True)
+        print(f"Wrote {outfile}")
 
-        logic = np.zeros((signal.shape[0], signal.shape[1]))
-        print(len(logic), np.shape(logic))
+    pred_noise_af.close() 
 
-        #sys.exit(0)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--datadir",
+                        help="Path to science corrtags")
+    parser.add_argument("-n", "--noise",
+                        help="Predicted noise level file")
+    parser.add_argument("-o", "--outdir", default=".",
+                        help="Output directory")
+    args = parser.parse_args()
+    corrtags = glob.glob(os.path.join(args.datadir, "*corrtag*fits"))
 
-        for i in range (0, len(data['XCORR'][:])):
-                if (data['XCORR'][i] > xstart) and (data['XCORR'][i] < xend-6):
-                        if (data['YCORR'][i] > ystart) and (data['YCORR'][i] < yend) and (data['PHA'][i] < 28) and (data['PHA'][i] > 3):
-                                jj = int ((data['XCORR'][i] - xstart) / 6.0 / 8.0 * fact)
-                                kk = int ((data['YCORR'][i] - ystart) / 12.0 / 2.0)
-
-                                #ss = int(data['PHA'][i] / 2.0)
-                                #print('jj, kk ', jj, kk )
-
-                                delta_eps = noise[kk][int(jj / fact)] / float(fact)
-                                logic [kk][jj] = 1
-                                delta_eps_ind = delta_eps / float(n_rec[kk, jj])
-
-                                wwf.write(str(data['EPSILON'][i]) + '\t' + str(delta_eps) + '\t' + str(delta_eps_ind) + '\n')
-                                data['EPSILON'][i] = data['EPSILON'][i] - delta_eps_ind
-
-        #hdul.writeto('lcm802inq_corrtag_a_after_correction.fits')
-
-        plt.imshow(logic)
-        plt.show()
-
-        print(logic.shape[0])
-        #sys.exit(0)
-
-        nmb_zero = logic.shape[0] * logic.shape[1] - np.sum(logic)
-
-        if nmb_zero > 1:
-                new_records = data[len(data)-int(nmb_zero):].copy()
-        print('Null records: ', nmb_zero, ' copied: ', len(new_records))
-
-        #sys.exit(0)
-
-        cnt = 0
-        for i in range (0, logic.shape[0]):
-                for j in range (0, logic.shape[1]):
-                        if logic[i,j] == 0:
-                                y_coord = 12.0 * 2.0 * i       + ystart
-                                x_coord = 6.0 * 8.0 * j / fact + xstart
-
-                                new_records['RAWX'][cnt] = x_coord
-                                new_records['RAWY'][cnt] = y_coord
-
-                                new_records['XCORR'][cnt] = x_coord
-                                new_records['YCORR'][cnt] = y_coord
-
-                                new_records['XFULL'][cnt] = x_coord
-                                new_records['YFULL'][cnt] = y_coord
-
-                                new_records['EPSILON'][cnt] = -noise[i][int(j / fact)] / fact
-                                new_records['PHA'][cnt] = 10
-
-                                cnt = cnt + 1
-
-        data = np.concatenate ([data, new_records])	
-        hdul[1].data = data
-        hdul.writeto(f'{rootdir}corrected_std/{rootn}_corrtag_a.fits')
-                                #val1[kk, jj, ss] = val1[kk, jj, ss] + data['EPSILON'][i]
-
-        #plt.plot (cmb)
-        #plt.plot (signal)
-        #plt.show()
-
-        #noise_0 = np.zeros(len(noise))
-
-        #cmb, clean = combined (list_of_mu, list_of_gaussian, noise_0)
-
-        #plt.plot (cmb)
-        #plt.plot (signal)
-        #plt.show()
-
-        #np.save ('clean', cmb)
+    main(corrtags, args.noise, outdir=args.outdir)
 
