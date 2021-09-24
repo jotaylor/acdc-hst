2b5693ff31bed90e45adf78bd1f19bb57c5343fa
Jo Taylor <jotaylor@stsci.edu>
2021-09-24 17:40:45 -0400

    Rewrite query to darkevents so that individual HV tables are used. Store each DB's results in a DF in a dict. Improve bin_data func

--- hv_vs_modalgain.py
+++ hv_vs_modalgain.py
@@ -3,66 +3,84 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
 from sqlalchemy.orm import load_only
+from timeit import default_timer as timer
 import pandas as pd
 from collections import OrderedDict,defaultdict
 
 from connect_db import load_connection
-from darkevents_schema import DarkEvents
+import darkevents_schema
+from utils import sql_to_df
 
 HV = {"FUVA": [163, 167, 169, 171, 173, 178],
       "FUVB": [163, 167, 169, 175]}
 ALL_PHAS = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+DBNAME = "dark_events"
+TIMING = True
 
 def query_data(segment="*", mjdstart=0, mjdend=99999, x0=0, x1=16385, 
-               y0=0, y1=1025, hv=163, getall=False, dbname="dark_events"):
+               y0=0, y1=1025, hv=163, getall=True):
 
     # Connect to database
-    with open("settings.yaml", "r") as f:
-        settings = yaml.load(f, Loader=yaml.SafeLoader)
-        dbsettings = settings["dbsettings"][dbname]
-    session, engine = load_connection(dbsettings)
+    session, engine = load_connection("dark_events")
     
     # Define columns to return from database
     cols = ["pha", "hv", "segment"]
-    
-    # Execute query
-    if getall is True:
-        query = session.query(DarkEvents).options(load_only(*cols))\
-                .filter(DarkEvents.hv == hv)
-    else:
-        query = session.query(DarkEvents).options(load_only(*cols))\
-                .filter(DarkEvents.segment == segment)\
-                .filter(DarkEvents.hv == hv)\
-                .filter(DarkEvents.mjd > mjdstart)\
-                .filter(DarkEvents.mjd < mjdend)\
-                .filter(DarkEvents.xcorr > x0)\
-                .filter(DarkEvents.xcorr < x1)\
-                .filter(DarkEvents.ycorr > y0)\
-                .filter(DarkEvents.ycorr < y1)
-    results = query.all()
 
-    # Convert query results (list of attributes) to pandas dataframe
-    d = {col: [getattr(x, col) for x in results] for col in cols}
-    df = pd.DataFrame(d)
+    # Get Tables for all HVs
+#    possible_hvs = [163, 167, 169, 171, 173, 175, 178]
+    possible_hvs = [171, 173]
+    data = {}
+    for hv in possible_hvs:
+        tablename = f"DarkEventsHv{hv}"
+        events_table = getattr(darkevents_schema, tablename)
+        # Execute query
+        query0 = timer()
+        if getall is True:
+            query = session.query(events_table).options(load_only(*cols))
+        else:
+            query = session.query(events_table).options(load_only(*cols))\
+                    .filter(events_table.segment == segment)\
+                    .filter(events_table.hv == hv)\
+                    .filter(events_table.mjd > mjdstart)\
+                    .filter(events_table.mjd < mjdend)\
+                    .filter(events_table.xcorr > x0)\
+                    .filter(events_table.xcorr < x1)\
+                    .filter(events_table.ycorr > y0)\
+                    .filter(events_table.ycorr < y1)
+        results = query.all()
+        query1 = timer()
 
-    return df
+        # Convert query results (list of attributes) to pandas dataframe
+        df = sql_to_df(results, cols)
+        data[hv] = df
+        processing1 = timer()
+        if TIMING is True:
+            print(f"Query for HV={hv} took {query1-query0:.3g} seconds")
+            print(f"Data formatting for HV={hv} took {processing1-query1:.3g} seconds")
 
-def bin_data(df, segment):
+    return data
+
+def bin_data(data, segment, interactive=False):
     x = []
     y = []
     hvs_gain = OrderedDict()
     for hv in HV[segment]:
-        phas = df.loc[df["hv"] == hv].pha.values
+        df = data[hv] 
+        phas = df.pha.values
         s_phas = list(set(phas))
         present = [x for x in ALL_PHAS if x in s_phas]
         if set(present) != set(ALL_PHAS):
             print(f"HV={hv} does not have counts at all PHAs")
             continue
-        pl.hist(phas, bins=np.arange(0,32))
-        pl.show()
-        x0 = int(input("Enter starting X for normal fit: "))
-        x1 = int(input("Enter ending X for normal fit: "))
-        pl.clf()
+        if interactive is True:
+            pl.hist(phas, bins=np.arange(0,32))
+            pl.show()
+            x0 = int(input("Enter starting X for normal fit: "))
+            x1 = int(input("Enter ending X for normal fit: "))
+            pl.clf()
+        else:
+            x0 = 5
+            x1 = 15
         subset = np.where((phas > x0) & (phas < x1))
         mean,std = norm.fit(phas[subset])
         modal_gain = mean
