0e6cae0767926f5b4ca7ae66e2c71f4ac33ff2ef
Jo Taylor <jotaylor@stsci.edu>
2021-09-24 17:43:57 -0400

    Initial commit

---
+++ darkevents_schema.py
@@ -0,0 +1,132 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import Float, ForeignKey, Integer, String
+
+Base = declarative_base()
+
+class DarkEvents(Base):
+    """Object-relational mapping for the DarkEvents table."""
+
+    __tablename__ = "darkevents"
+    id = Column(Integer, primary_key=True, nullable=False)
+    xcorr = Column(Float())
+    ycorr = Column(Float())
+    pha = Column(Integer())
+    mjd = Column(Float(precision='15,9'))
+    hv = Column(Integer())
+    segment = Column(String(5))
+    filename = Column(String(30))
+    proposid = Column(Integer())
+
+class DarkEventsHv163(Base):
+    """Object-relational mapping for the DarkEvents table."""
+
+    __tablename__ = "darkeventshv163"
+    id = Column(Integer, primary_key=True, nullable=False)
+    xcorr = Column(Float())
+    ycorr = Column(Float())
+    pha = Column(Integer())
+    mjd = Column(Float(precision='15,9'))
+    hv = Column(Integer())
+    segment = Column(String(5))
+    filename = Column(String(30))
+    proposid = Column(Integer())
+
+class DarkEventsHv167(Base):
+    """Object-relational mapping for the DarkEvents table."""
+
+    __tablename__ = "darkeventshv167"
+    id = Column(Integer, primary_key=True, nullable=False)
+    xcorr = Column(Float())
+    ycorr = Column(Float())
+    pha = Column(Integer())
+    mjd = Column(Float(precision='15,9'))
+    hv = Column(Integer())
+    segment = Column(String(5))
+    filename = Column(String(30))
+    proposid = Column(Integer())
+
+class DarkEventsHv169(Base):
+    """Object-relational mapping for the DarkEvents table."""
+
+    __tablename__ = "darkeventshv169"
+    id = Column(Integer, primary_key=True, nullable=False)
+    xcorr = Column(Float())
+    ycorr = Column(Float())
+    pha = Column(Integer())
+    mjd = Column(Float(precision='15,9'))
+    hv = Column(Integer())
+    segment = Column(String(5))
+    filename = Column(String(30))
+    proposid = Column(Integer())
+
+class DarkEventsHv171(Base):
+    """Object-relational mapping for the DarkEvents table."""
+
+    __tablename__ = "darkeventshv171"
+    id = Column(Integer, primary_key=True, nullable=False)
+    xcorr = Column(Float())
+    ycorr = Column(Float())
+    pha = Column(Integer())
+    mjd = Column(Float(precision='15,9'))
+    hv = Column(Integer())
+    segment = Column(String(5))
+    filename = Column(String(30))
+    proposid = Column(Integer())
+
+class DarkEventsHv173(Base):
+    """Object-relational mapping for the DarkEvents table."""
+
+    __tablename__ = "darkeventshv173"
+    id = Column(Integer, primary_key=True, nullable=False)
+    xcorr = Column(Float())
+    ycorr = Column(Float())
+    pha = Column(Integer())
+    mjd = Column(Float(precision='15,9'))
+    hv = Column(Integer())
+    segment = Column(String(5))
+    filename = Column(String(30))
+    proposid = Column(Integer())
+
+class DarkEventsHv175(Base):
+    """Object-relational mapping for the DarkEvents table."""
+
+    __tablename__ = "darkeventshv175"
+    id = Column(Integer, primary_key=True, nullable=False)
+    xcorr = Column(Float())
+    ycorr = Column(Float())
+    pha = Column(Integer())
+    mjd = Column(Float(precision='15,9'))
+    hv = Column(Integer())
+    segment = Column(String(5))
+    filename = Column(String(30))
+    proposid = Column(Integer())
+
+class DarkEventsHv178(Base):
+    """Object-relational mapping for the DarkEvents table."""
+
+    __tablename__ = "darkeventshv178"
+    id = Column(Integer, primary_key=True, nullable=False)
+    xcorr = Column(Float())
+    ycorr = Column(Float())
+    pha = Column(Integer())
+    mjd = Column(Float(precision='15,9'))
+    hv = Column(Integer())
+    segment = Column(String(5))
+    filename = Column(String(30))
+    proposid = Column(Integer())
+
+
+class DarkEventsHvOther(Base):
+    """Object-relational mapping for the DarkEvents table."""
+
+    __tablename__ = "darkeventshvother"
+    id = Column(Integer, primary_key=True, nullable=False)
+    xcorr = Column(Float())
+    ycorr = Column(Float())
+    pha = Column(Integer())
+    mjd = Column(Float(precision='15,9'))
+    hv = Column(Integer())
+    segment = Column(String(5))
+    filename = Column(String(30))
+    proposid = Column(Integer())
