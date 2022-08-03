ee37037dc5ecf81a5751a49dc6e2080568187cf4
Jo Taylor <jotaylor@stsci.edu>
2022-08-03 18:01:55 -0400

    Add LyA filtering option, and user supplied superdarks

--- acdc/run_acdc.py
+++ acdc/run_acdc.py
@@ -2,8 +2,9 @@ import argparse
 from acdc.dark_correction import Acdc
 
 
-def run_acdc(indir, darkcorr_outdir, x1d_outdir=None, binned=False, hv=None, 
-             segment=None, overwrite=False):
+def run_acdc(indir, darkcorr_outdir, x1d_outdir=None, lo_darkname=None, 
+             hi_darkname=None, binned=False, hv=None, 
+             segment=None, overwrite=False, exclude_lya=False):
     """Wrapper script to perform custom dakr correction on an input directory.
     
     Args:
@@ -15,10 +16,12 @@ def run_acdc(indir, darkcorr_outdir, x1d_outdir=None, binned=False, hv=None,
         segment (str): (Optional) Process corrtags of this segment only.
     """
     A = Acdc(indir=indir, darkcorr_outdir=darkcorr_outdir, x1d_outdir=x1d_outdir,
-             binned=binned, segment=segment, hv=hv, overwrite=overwrite)
+             lo_darkname=lo_darkname, hi_darkname=hi_darkname,
+             binned=binned, segment=segment, hv=hv, overwrite=overwrite,
+             exclude_lya=exclude_lya)
     A.custom_dark_correction()
     A.calibrate_corrtags()
-
+    print(f"\nFINISHED: Products in {A.x1d_outdir}")
 
 def acdc_parser():
     parser = argparse.ArgumentParser()
@@ -28,6 +31,10 @@ def acdc_parser():
                         help="Name of directory to write model superdarks and custom corrtags to")
     parser.add_argument("--x1d_outdir", default=None,
                         help="Name of directory to write 1D spectra to")
+    parser.add_argument("--lo", dest="lo_darkname",
+                        help="Name of low activity superdark")
+    parser.add_argument("--hi", dest="hi_darkname",
+                        help="Name of high activity superdark")
     parser.add_argument("--binned", default=False,
                         action="store_true",
                         help="Toggle to indicate that supplied superdarks are binned")
@@ -38,9 +45,13 @@ def acdc_parser():
                         help="HV to filter corrtags by")
     parser.add_argument("--segment", default=None,
                         help="Segment to filter corrtags by")
+    parser.add_argument("--lya", default=False, dest="exclude_lya",
+                        action="store_true",
+                        help="Toggle to exclude LyA from science data scaling") 
     args = parser.parse_args()
-    run_acdc(args.indir, args.darkcorr_outdir, args.x1d_outdir, args.binned, args.hv, 
-             args.segment, args.clobber)
+    run_acdc(args.indir, args.darkcorr_outdir, args.x1d_outdir, 
+             args.lo_darkname, args.hi_darkname, args.binned, args.hv, 
+             args.segment, args.clobber, args.exclude_lya)
 
 
 if __name__ == "__main__":
