5b7e9c8fb7f09bb2bda4e08593a8199e8ccf44d5
Jo Taylor <jotaylor@stsci.edu>
2023-07-21 16:22:51 -0400

    Move the function that determines the PSA/WCA regions into utils

--- acdc/utils/utils.py
+++ acdc/utils/utils.py
@@ -1,6 +1,7 @@
 import datetime
 import numpy as np
 import pandas as pd
+from acdc.database.calculate_dark import get_aperture_region
 
 def sql_to_df(sql_results, returncols):
     # Convert query results (list of attributes) to pandas dataframe
@@ -101,4 +102,42 @@ def unbin_image(binned_im, bin_x, bin_y, xstart=0, ystart=0, xend=16384, yend=10
         for j in range(len(ys)-1):
             unbinned_im[ys[j]:ys[j+1], xs[i]:xs[i+1]] = im_perpixel[j,i]
     return unbinned_im
+
+def get_psa_wca(segment, cenwave, lp, binning, pad_psa=[0,0], pad_wca=[0,0]):
+    """
+    Determine the rows that correspond to the PSA and WCA apertures for a 
+    given segment, cenwave, and lifetime position. Return the row indices in 
+    the binned superdark coordinate system.
+
+    Arguments:
+        segment (str): Get PSA/WCA regions for this segment.
+        cenwave (str): Get PSA/WCA regions for this cenwave.
+        lp (str): Get PSA/WCA regions for this lifetime position.
+        binning (dict): Dictionary that describes the binning information
+            in both the spatial and PHA dimensions.
+    Returns:
+        excluded_rows (array): List of rows to exclude. Corresponds
+            to the PSA and WCA regions.
+        apertures (dict): The excluded rows for each aperture, PSA and WCA.
+    """
+
+    excluded_rows = np.array(())
+    apertures = {"PSA": None, "WCA": None}
+    pad = {"PSA": pad_psa, "WCA": pad_wca}
+    for aperture in apertures:
+        aperture_regions = get_aperture_region(cenwave=cenwave, aperture=aperture, segments=[segment], life_adj=[lp])
+        box = aperture_regions[segment][f"lp{lp}_{aperture.lower()}_{cenwave}"]
+        xmin0, xmax0, ymin0, ymax0 = box                            
+        xsnew, ysnew = bin_coords(np.array([xmin0, xmax0]), np.array([ymin0, ymax0]), binning["bin_x"], binning["bin_y"], binning["xstart"], binning["ystart"], as_float=True, make_int=False)
+#        ap_xmin, ap_xmax = xsnew # We don't need x coords
+        ap_ymin, ap_ymax = ysnew
+        ap_ymin = int(np.floor(ap_ymin))
+        ap_ymax = int(np.ceil(ap_ymax))
+        ap_ymin -= pad[aperture][0]
+        ap_ymax += pad[aperture][1]
+        rows = np.arange(ap_ymin, ap_ymax)
+        apertures[aperture] = rows
+        excluded_rows = np.concatenate((excluded_rows, rows))
+    excluded_rows = excluded_rows.astype(int)
+    return excluded_rows, apertures
 
