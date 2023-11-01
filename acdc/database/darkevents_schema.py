b2293e7e6155346d984fd3f50af1d36ed3541b66
Jo Taylor <jotaylor@stsci.edu>
2023-11-01 15:29:53 -0400

    Use correct version of Decimal SQL column for MYSQL DB

--- acdc/database/darkevents_schema.py
+++ acdc/database/darkevents_schema.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column
-from sqlalchemy import Decimal, ForeignKey, Integer, String
+from sqlalchemy import Float, ForeignKey, Integer, String
+from sqlalchemy.types import DECIMAL as Decimal
 
 Base = declarative_base()
 
